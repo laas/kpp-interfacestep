9eed1e578c532bd3d5b2f42f8594eb11c318e869
Florent Lamiraux <florent@laas.fr>
2011-12-31 14:42:31 +0100

    Fix warning
        Remove extra semi-colon after KIT_PREDEF_CLASS.

--- include/kpp/interfacestep/fwd.hh
+++ include/kpp/interfacestep/fwd.hh
@@ -2,17 +2,17 @@
 # define KPP_INTERFACE_STEP_FWD_HH
 # include <KineoUtility/kitDefine.h>
 
-KIT_PREDEF_CLASS (CkppUICommand);
+KIT_PREDEF_CLASS (CkppUICommand)
 
 namespace kpp
 {
   namespace interfaceStep
   {
-    KIT_PREDEF_CLASS (Interface);
-    KIT_PREDEF_CLASS (CommandSetInitGoal);
-    KIT_PREDEF_CLASS (CommandDynamicPath);
-    KIT_PREDEF_CLASS (CommandRemoveConstraints);
-    KIT_PREDEF_CLASS (CommandGrabObject);
+    KIT_PREDEF_CLASS (Interface)
+    KIT_PREDEF_CLASS (CommandSetInitGoal)
+    KIT_PREDEF_CLASS (CommandDynamicPath)
+    KIT_PREDEF_CLASS (CommandRemoveConstraints)
+    KIT_PREDEF_CLASS (CommandGrabObject)
   } 
 } // end of namespace kpp.
 
